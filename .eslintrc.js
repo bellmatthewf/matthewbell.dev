c58f94cf77aaa52d5fe08cb6c289f0c14ada310f
bellmatthewf <41313531+bellmatthewf@users.noreply.github.com>
2020-12-25 18:38:13 -0500

    Add vuetify

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,15 @@ module.exports = {
         node: true
     },
     extends: [
-        'plugin:vue/essential',
-        '@vue/standard'
+        "plugin:vue/essential",
+        "@vue/standard"
     ],
     parserOptions: {
-        parser: 'babel-eslint'
+        parser: "babel-eslint"
     },
     rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
 
         indent: ["error", 4],
         semi: ["error", "always"],
@@ -22,12 +22,12 @@ module.exports = {
     overrides: [
         {
             files: [
-                '**/__tests__/*.{j,t}s?(x)',
-                '**/tests/unit/**/*.spec.{j,t}s?(x)'
+                "**/__tests__/*.{j,t}s?(x)",
+                "**/tests/unit/**/*.spec.{j,t}s?(x)"
             ],
             env: {
                 jest: true
             }
         }
     ]
-}
+};
