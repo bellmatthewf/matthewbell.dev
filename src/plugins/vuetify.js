53d0eec3561cc5e93e8260a2dceebe0dcbc2e2da
bellmatthewf <41313531+bellmatthewf@users.noreply.github.com>
2020-12-25 18:41:55 -0500

    Modify vuetify config

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,25 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
+import colors from "vuetify/lib/util/colors";
+
+const vuetify = new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                // primary: colors.purple.base,
+                // secondary: colors.grey.darken1,
+                // accent: colors.shades.black,
+                // error: colors.red.accent3,
+                background: colors.blueGrey.lighten5 // Not automatically applied
+            },
+            dark: {
+                // primary: colors.blue.lighten3,
+                // background: colors.indigo.base // If not using lighten/darken, use base to return hex
+            }
+        }
+    }
+});
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
-});
+export default vuetify;
