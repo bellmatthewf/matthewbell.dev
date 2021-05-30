19362e7aa1dd777a6cc26a2d5590c3103598665d
bellmatthewf <41313531+bellmatthewf@users.noreply.github.com>
2021-05-29 23:34:55 -0400

    Add themes

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -2,45 +2,60 @@
     <v-app-bar
         app
         flat
-        class="app-bar white pa-5"
+        class="app-bar transparent"
     >
         <div class="d-flex align-center">
-            <v-img
-                alt="Vuetify Logo"
-                class="shrink mr-2"
-                contain
-                src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-                transition="scale-transition"
-                width="40"
-            />
+            <a :href="routes.home">
+                <v-img
+                    alt="Vuetify Logo"
+                    class="shrink mr-2"
+                    contain
+                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+                    transition="scale-transition"
+                    width="40"
+                />
+            </a>
 
-            <v-img
-                alt="Vuetify Name"
-                class="shrink mt-1 hidden-sm-and-down"
-                contain
-                min-width="100"
-                src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-                width="100"
-            />
+            <a :href="routes.home">
+                <v-img
+                    alt="Vuetify Name"
+                    class="shrink mt-1 hidden-sm-and-down"
+                    contain
+                    min-width="100"
+                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+                    width="100"
+                />
+            </a>
+
         </div>
 
         <v-spacer></v-spacer>
+        <div>
+            <ul>
+                <v-btn
+                    plain
+                    exact
+                    v-for="btn in navBtns"
+                    :ripple="false"
+                    :key=btn.name
+                    :href=btn.url
+                    :target="btn.icon ? '_blank' : ''"
+                    :rel="btn.icon ? 'noopener noreferrer' : ''"
+                >
+                    <v-icon v-if="btn.icon">{{btn.icon}}</v-icon>
+                    <span v-else>{{btn.text}}</span>
+                </v-btn>
+                <v-btn
+                    plain
+                    exact
+                    :ripple="false"
+                    @click="toggleDarkMode"
+                >
+                    <v-icon>{{lightOrDarkIcon}}</v-icon>
+                </v-btn>
+            </ul>
+        </div>
 
-        <ul>
-            <v-btn
-                plain
-                exact
-                class="transparent black--text"
-                v-for="btn in navBtns"
-                :key=btn.name
-                :href=btn.url
-                :target="btn.icon ? '_blank' : ''"
-                :rel="btn.icon ? 'noopener noreferrer' : ''"
-            >
-                <v-icon v-if="btn.icon">{{btn.icon}}</v-icon>
-                <span v-else>{{btn.text}}</span>
-            </v-btn>
-        </ul>
     </v-app-bar>
 </template>
 
@@ -51,16 +66,33 @@ export default {
     name: "TheNavBar",
     data () {
         return {
+            routes,
             navBtns: [
                 { text: "Home", url: routes.home },
                 { text: "Blog", url: routes.blog },
                 { text: "Contact", url: routes.contact },
                 { icon: "mdi-github", url: "https://github.com/bellmatthewf" },
                 { icon: "mdi-linkedin", url: "https://www.linkedin.com/in/matthewfbell/" },
-                { icon: "mdi-weather-night" },
-                { icon: "mdi-white-balance-sunny" },
             ],
         };
+    },
+    computed: {
+        isDark () {
+            return this.$vuetify.theme.dark;
+        },
+        lightOrDarkIcon () {
+            if (this.isDark) {
+                return "mdi-white-balance-sunny";
+            } else {
+                return "mdi-weather-night";
+            }
+        },
+    },
+    methods: {
+        toggleDarkMode () {
+            this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+            window.localStorage.setItem("vue-dark-mode", this.$vuetify.theme.dark);
+        },
     },
 };
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,19 +2,30 @@ import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
 import colors from "vuetify/lib/util/colors";
 
+function getDarkMode () {
+    const storedDarkMode = window.localStorage.getItem("vue-dark-mode");
+
+    if (storedDarkMode) {
+        return storedDarkMode;
+    } else {
+        return false;
+    }
+}
+
 const vuetify = new Vuetify({
     theme: {
+        dark: getDarkMode(),
         themes: {
             light: {
                 // primary: colors.purple.base,
                 // secondary: colors.grey.darken1,
                 // accent: colors.shades.black,
                 // error: colors.red.accent3,
-                background: colors.blueGrey.lighten5, // Not automatically applied
+                // background: colors.blueGrey.lighten5, // Not automatically applied
             },
             dark: {
                 // primary: colors.blue.lighten3,
-                // background: colors.indigo.base // If not using lighten/darken, use base to return hex
+                background: colors.blue.base, // If not using lighten/darken, use base to return hex
             },
         },
     },
