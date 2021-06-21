0b14ef87516344b91b31ecd69cfb1d44d9b8fc4e
bellmatthewf <41313531+bellmatthewf@users.noreply.github.com>
2021-06-20 23:52:14 -0400

    Add blog post redirection on bad url

--- src/views/BlogPost.vue
+++ src/views/BlogPost.vue
@@ -3,7 +3,7 @@
         <h1 class="mb-3">{{fmAttributes.title}}</h1>
         <p class="mb-7 subtitle-1 divergent-5--text">{{fmAttributes.date}} - {{fmAttributes.duration}}</p>
         <div
-            v-html="md"
+            v-html="renderedMd"
             class="blog-post mb-16"
         ></div>
     </div>
@@ -20,18 +20,33 @@ export default {
     },
     data () {
         return {
-            md: undefined,
+            renderedMd: undefined,
             fmAttributes: {},
         };
     },
     async created () {
-        const postPath = `${process.env.VUE_APP_DOMAIN}/posts/blog/${this.$route.params.filename}.md`;
-        const md = await fetch(postPath);
-        const res = await md.text();
-        const fmContent = fm(res);
-
-        this.fmAttributes = fmContent.attributes;
-        this.md = marked(fmContent.body);
+        const md = await this.fetchPost();
+        const fmContent = this.loadMarkdown(md);
+        this.redirectIfInvalidPost(fmContent);
+    },
+    methods: {
+        async fetchPost () {
+            const postPath = `${process.env.VUE_APP_DOMAIN}/posts/blog/${this.$route.params.filename}.md`;
+            const res = await fetch(postPath);
+            const md = await res.text();
+            return md;
+        },
+        loadMarkdown (md) {
+            const fmContent = fm(md);
+            this.fmAttributes = fmContent.attributes;
+            this.renderedMd = marked(fmContent.body);
+            return fmContent;
+        },
+        redirectIfInvalidPost (fmContent) {
+            if (Object.keys(fmContent.attributes).length === 0) {
+                this.$router.push({ name: "PageNotFound" });
+            }
+        },
     },
 };
 </script>
